29a7f7bb91d7aaf5d3e2de5313e55812a3ff0a39
huhn511 <sehe89@gmail.com>
2023-04-22 20:17:30 +0200

    fix logo

--- src/components/LogoTestnet.tsx
+++ src/components/LogoTestnet.tsx
@@ -149,7 +149,7 @@ export const LogoTestnet: React.FC<BoxProps> = (props) => {
               filterUnits="userSpaceOnUse"
               color-interpolation-filters="sRGB"
             >
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feFlood floodOpacity="0" result="BackgroundImageFix" />
               <feColorMatrix
                 in="SourceAlpha"
                 type="matrix"
